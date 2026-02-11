a648ab0a41a2653cebdde53872c3b5b58bdb14b1
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2026-02-11 19:18:20 +0000

    style: add Prettier configuration and format script.js / index.jtml

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds } from "./storage.js"
+import { getUserIds } from "./storage.js";
 
 window.onload = function () {
- const users = getUserIds()
-  document.querySelector("body").innerText = `There are ${users.length} users`
-}
+  const users = getUserIds();
+  document.querySelector("body").innerText = `There are ${users.length} users`;
+};
