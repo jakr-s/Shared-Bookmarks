0ace47ac0f9b6a7bddaec9255a9d67e4a15e2d35
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2026-02-12 00:34:47 +0000

    feat: add DOM elements to script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 import { getUserIds, getData, setData, clearData } from "./storage.js";
+
+const userSelect = document.querySelector("#user-select");
+const bookmarksList = document.querySelector("#bookmarks-list");
+const validationMessage = document.querySelector("#validation-message");
+const bookmarkForm = document.querySelector("#bookmark-form");
+const bookmarkUrl = document.querySelector("#bookmark-url");
+const bookmarkTitle = document.querySelector("#bookmark-title");
+const bookmarkDescription = document.querySelector("#bookmark-description");
